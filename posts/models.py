5f3142e966bd19f2a8a192c84c8e5b264ab4c87e
Durantay Quentin <quentin.durantay@gmail.com>
2017-09-12 14:00:25 +0200

    Add blog post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    pub_date = models.DateTimeField()
+    image = models.ImageField(upload_to='media/')
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.title)
